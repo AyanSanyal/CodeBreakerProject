1138dd24fc04dba9a67851cb6a93f17a81aeee2f
Ayan Sanyal <M1039183@mindtree.com>
2017-02-19 00:03:52 +0530

    fourth step

--- src/assets/main.js
+++ src/assets/main.js
@@ -35,3 +35,17 @@ function validateInput(input) {
     } else
         return true;
 }
+
+function getResults(input) {
+    let html = '<div class="row"><span class="col-md-6">' + input + '</span><div class="col-md-6">';
+    for (i = 0; i < input.length; i++) {
+        if (input.charAt(i) == answer.value.charAt(i)) {
+            html += '<span class="glyphicon glyphicon-ok"></span>';
+        }
+        if (input.charAt(i) == answer.value.charAt(i) > -1) {
+            html += '<span class="glyphicon glyphicon-transfer"></span>';
+        } else {
+            html += '';
+        }
+    }
+}
