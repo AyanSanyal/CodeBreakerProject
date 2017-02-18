8400d580498ba398075193665ca51d6d32154128
Ayan Sanyal <M1039183@mindtree.com>
2017-02-19 00:10:01 +0530

    fourth step

--- src/assets/main.js
+++ src/assets/main.js
@@ -41,7 +41,7 @@ function getResults(input) {
     for (i = 0; i < input.length; i++) {
         if (input.charAt(i) == answer.value.charAt(i)) {
             html += '<span class="glyphicon glyphicon-ok"></span>';
-        } else if (answer.value.indexOf(input.chatAt(i)) > -1) {
+        } else if (answer.value.indexOf(input.charAt(i)) > -1) {
             html += '<span class="glyphicon glyphicon-transfer"></span>';
         } else {
             html += '';
